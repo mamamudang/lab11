454e8331ca30a78913bd1056aab267964275cb05
mamamudang <humlake7.66@gmail.com>
2024-12-20 19:12:05 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,23 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    string predictedGrade = grades[rand() % 9];
+
+    cout << "You will get " << predictedGrade << " in this 261102." << endl;
+
+    return 0;
+}
